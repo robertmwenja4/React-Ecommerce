b931271f56080addae0f67f4e3737b0ad510bcf6
robertmwenja4 <robertmwenja4@gmail.com>
2022-08-19 21:47:50 +0300

    Persist LOgin

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const BASE_URL = "http://localhost:5000/api";
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZGM0Y2EyMDFhZWIwYWZiNWMzNzYxOCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1OTAwMTgyMywiZXhwIjoxNjU5MjYxMDIzfQ.4H6CUemy1nn8hFH-dSl-gdXnwZBcqetG-BorVaEaXWU";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
