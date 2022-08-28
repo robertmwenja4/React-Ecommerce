26cde68a42baf0bcf4734f2cae43a0807f3308d4
robertmwenja4 <robertmwenja4@gmail.com>
2022-08-28 22:28:59 +0300

    Check Token

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 
 const BASE_URL = "http://localhost:5000/api";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;;
+//JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken || 
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZGM0Y2EyMDFhZWIwYWZiNWMzNzYxOCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MTY2NjA4OSwiZXhwIjoxNjYxOTI1Mjg5fQ.iuPsiKrH9qBf0jVWzNU-yzuZTTorxr8_K8IdvLJ_DEI";
 if (TOKEN === null) {
     console.log("Empty token");
 }
